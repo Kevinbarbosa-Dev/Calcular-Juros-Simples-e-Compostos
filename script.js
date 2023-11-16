1e0bd4e992dcd4a039a4086781880735960d4a47
Kevin Barbosa Braga <100783508+Kevinbarbosa-Dev@users.noreply.github.com>
2023-11-16 13:35:14 -0300

    atualização

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ document.getElementById('montante').addEventListener('click', function () {
     let prazo = parseFloat(document.getElementById('prazo').value);
     let anoFormatado = prazo / 12;
 
+    if(isNaN(ini) && isNaN(juros) && isNaN(prazo)){
+        conclusão.innerHTML = `Insira os dados`;
+        conclusão.style.color = 'red'
+    }
+
     let res = ini * (1 + juros) ** prazo;
     let resultadoFormatado = res.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     document.getElementById('res').innerHTML = resultadoFormatado;
 
     if (anoFormatado > 1) {
         document.getElementById('conclusão').innerHTML = `Em ${anoFormatado} anos você terá:`;
+        conclusão.style.color = 'white'
     } else {
         document.getElementById('conclusão').innerHTML = `Em ${anoFormatado} ano você terá:`;
     }
+
+    
 });
+/* se prazo for 1 até 11, então retornará mês ou meses
+
+*/
