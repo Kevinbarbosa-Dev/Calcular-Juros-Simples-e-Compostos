bbebb84a4f1b95c40e91e93300493d6559aa85f7
Kevinbarbosa <kevinbarbosa5262@gmail.com>
2023-12-21 17:34:52 -0300

    ainda consertando

--- script.js
+++ script.js
@@ -4,25 +4,20 @@ document.getElementById('montante').addEventListener('click', function () {
     let juros = parseFloat(document.getElementById('per').value) / 100;
     let prazo = parseFloat(document.getElementById('prazo').value);
     let anoFormatado = prazo / 12;
-
-    if(isNaN(ini) && isNaN(juros) && isNaN(prazo)){
-        conclusão.innerHTML = `Insira os dados`;
-        conclusão.style.color = 'red'
+    
+    if(ini.length === 0 || juros.length === 0 || prazo.length === 0){
+        document.getElementById('conclusão').innerHTML = `Insira os dados`;
+        document.getElementById('conclusão').style.color = 'red'
     }
 
     let res = ini * (1 + juros) ** prazo;
     let resultadoFormatado = res.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     document.getElementById('res').innerHTML = resultadoFormatado;
 
-    if (anoFormatado > 1) {
-        document.getElementById('conclusão').innerHTML = `Em ${anoFormatado} anos você terá:`;
-        conclusão.style.color = 'white'
-    } else {
-        document.getElementById('conclusão').innerHTML = `Em ${anoFormatado} ano você terá:`;
-    }
-
-    
-});
+    document.getElementById('conclusão').innerHTML = `Em ${anoFormatado} ${anoFormatado > 1 ? 'anos' : 'ano'} você terá:`;
+    conclusão.style.color = 'white';
+       
+});// return anoFormato ? < 1 
 /* se prazo for 1 até 11, então retornará mês ou meses
 
 */
